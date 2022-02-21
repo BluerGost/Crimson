14531f36d697fa820ae451c784fad0a1c0dde61d
Kevin Stephen Biswas <kevinstephenbiswas@gmial.com>
2022-02-21 17:25:41 +0900

    Added Api and WorkerService

---
+++ Crimson.WorkerService/Program.cs
@@ -0,0 +1,10 @@
+using Crimson.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
