2f69ddcb2c63d68f3a585726e61307d4f6d11fda
Kevin Stephen Biswas <kevinstephenbiswas@gmial.com>
2022-03-05 16:31:34 +0900

    Data access library added.

---
+++ DataAccess/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    internal interface IRepository
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+}
---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    internal class Repository : IRepository
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
